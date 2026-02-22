ee7155c7274562762acf4580a1aaf7cb555cc204
LuisMi1707 <as0939032023@unab.edu.sv>
2026-02-21 23:05:36 -0600

    feat: Se creó la funcion de agregar producto en app.js y se integró al index.html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ class Product {
         this.id = Date.now();
         this.nombre = nombre;
         this.precio = parseFloat(precio);
-        this.stock = parseInt(stock);
+        this.stock = parseInt(stock) || 0;
         this.descripcion = descripcion;
     }
 }
@@ -14,4 +14,14 @@ let productos = [
     new Product("Mouse", 25, 5, "Inalámbrico")
 ];
 
-
+function agregarProducto() {
+    // Obtener valores del formulario
+    const nombre = document.getElementById('nombre').value;
+    const precio = document.getElementById('precio').value;
+    
+    
+    // Crear y agregar producto (stock por defecto = 0)
+    const nuevo = new Product(nombre, precio, 0, "");
+    productos.push(nuevo);
+    
+}
